7b3897f9fd851a5eda58607f0a7dcaadc4d47204
eqrw105 <eqrw105@naver.com>
2025-08-10 14:44:06 +0900

    build: status-pages 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation(libs.ktor.server.config.yaml)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.status.pages)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
