1b8036505dccf07134f300fd648146fdb7c49a04
eqrw105 <eqrw105@naver.com>
2025-08-23 17:44:14 +0900

    feat: 데이터베이스 적용 및 패키지 경로 수정

---
+++ src/main/kotlin/config/Databases.kt
@@ -0,0 +1,21 @@
+package config
+
+import infrastructure.table.UsersTable
+import io.ktor.server.application.Application
+import org.jetbrains.exposed.v1.jdbc.Database
+import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+
+fun Application.configureDatabases() {
+    val url = environment.config.property("ktor.database.url").getString()
+    val user = environment.config.property("ktor.database.user").getString()
+    val password = environment.config.property("ktor.database.password").getString()
+    Database.connect(
+        url = url,
+        user = user,
+        password = password,
+    )
+    transaction {
+        SchemaUtils.create(UsersTable)
+    }
+}
