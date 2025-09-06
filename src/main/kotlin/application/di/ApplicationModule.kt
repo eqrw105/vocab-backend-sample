b1477c9561c5208de732cc309f9dac9397691d4d
eqrw105 <eqrw105@naver.com>
2025-09-06 15:09:53 +0900

    feat: 게스트 유저 토큰 생성 유즈케이스 추가

--- src/main/kotlin/application/di/ApplicationModule.kt
+++ src/main/kotlin/application/di/ApplicationModule.kt
@@ -1,9 +1,11 @@
 package application.di
 
+import application.usecase.CreateGuestTokenUseCase
 import application.usecase.VerifyAppCheckTokenUseCase
 import org.koin.dsl.module
 
 val applicationModule =
     module {
         single { VerifyAppCheckTokenUseCase(get(), get()) }
+        single { CreateGuestTokenUseCase(get(), get(), get()) }
     }
