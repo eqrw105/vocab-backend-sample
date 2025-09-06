1448596efbd4766317a398897a864b62171dae85
eqrw105 <eqrw105@naver.com>
2025-09-06 15:45:11 +0900

    feat: 토큰 삭제 라우터 추가

--- src/main/kotlin/plugins/routing/SessionRoutes.kt
+++ src/main/kotlin/plugins/routing/SessionRoutes.kt
@@ -3,14 +3,18 @@ package plugins.routing
 import application.model.ApiException
 import application.usecase.CreateGuestTokenUseCase
 import application.usecase.RefreshTokenUseCase
+import application.usecase.RevokeTokenUseCase
 import io.ktor.http.HttpStatusCode
 import io.ktor.resources.Resource
 import io.ktor.server.auth.principal
 import io.ktor.server.request.receive
+import io.ktor.server.resources.delete
 import io.ktor.server.resources.post
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.application
+import kotlin.uuid.Uuid
+import kotlin.uuid.toJavaUuid
 import kotlinx.serialization.Serializable
 import org.koin.ktor.ext.inject
 import plugins.routing.dto.request.SessionRefreshRequestDTO
@@ -18,6 +22,8 @@ import plugins.routing.dto.response.TokenDTO
 import plugins.routing.dto.response.toDTO
 import plugins.routing.security.appCheck
 import plugins.routing.security.principal.AppCheckPrincipal
+import plugins.routing.security.principal.PrivatePrincipal
+import plugins.routing.security.private
 
 @Serializable
 @Resource("/v1/session")
@@ -26,6 +32,13 @@ class SessionV1 {
     @Resource("/refresh")
     class Refresh(
         val parent: SessionV1 = SessionV1(),
+    )
+
+    @Serializable
+    @Resource("/{tokenId}")
+    class Id(
+        val parent: SessionV1 = SessionV1(),
+        val tokenId: Uuid,
     )
 }
 
@@ -48,6 +61,24 @@ fun Route.sessionRoutes() {
             val refreshTokenUseCase by application.inject<RefreshTokenUseCase>()
             val result = refreshTokenUseCase(request.refreshToken)
             call.respond<TokenDTO>(HttpStatusCode.OK, result.toDTO())
+        }
+    }
+    private {
+        delete<SessionV1.Id> { route ->
+            val principal = call.principal<PrivatePrincipal>() ?: throw ApiException.UnauthorizedException()
+            val userId = principal.userId
+
+            val tokenId =
+                runCatching { route.tokenId.toJavaUuid() }.getOrNull()
+                    ?: throw ApiException.ValidationException("tokenId 형식이 올바르지 않습니다.")
+
+            val revokeTokenUseCase: RevokeTokenUseCase by application.inject<RevokeTokenUseCase>()
+            val deleted = revokeTokenUseCase(tokenId, userId)
+            if (deleted) {
+                call.respond(HttpStatusCode.NoContent)
+            } else {
+                throw ApiException.NotFoundException()
+            }
         }
     }
 }
